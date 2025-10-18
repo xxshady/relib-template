6752608d3dd7095d1aa9d10bdcae32c5c5ac0bd7
xshady <fastik036@gmail.com>
2025-10-18 21:12:57 +0300

    extend reference-counting safety condition with &'static types

--- host/src/main.rs
+++ host/src/main.rs
@@ -24,7 +24,7 @@ fn main() {
   // main function is unsafe to call (as well as any other module export) because these preconditions are not checked by relib:
   // 1. returned value must be actually `R` at runtime. For example if you called this function with type bool but module returns i32, UB will occur.
   // 2. type of return value must be FFI-safe.
-  // 3. returned value must not be a reference-counting pointer (see caveats on main docs page/README).
+  // 3. returned value must not be a reference-counting pointer or &'static T (see caveats on main docs page/README).
   let returned_value = unsafe { module.call_main::<()>() };
 
   // if module panics while executing any export it returns None
